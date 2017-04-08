f0e3153ee87f4a9a06e2c14b27e4c2a2cbb9d669
Nurbek Ismailov <nurbek82@gmail.com>
2017-04-08 11:25:28 -0700

    added data-author and url

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -15,9 +15,6 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-  var $source   = $('#blogTemplate').html();
-  var template = Handlebars.compile($source);
- console.log(template);
 
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
@@ -28,17 +25,20 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
- // console.log (this);
+  // console.log (this);
+  var $source   = $('#blogTemplate').html();
+  var template = Handlebars.compile($source);
+  console.log(template);
   return template(this);
-//   var data = {
-//     categorgy : this.category,
-//     title: this.title,
-//     author: this.author,
-//     publish: this.publishedOn,
-//   }
-// var html = template(data)
- // console.log(html);
-// return html;
+  //   var data = {
+  //     categorgy : this.category,
+  //     title: this.title,
+  //     author: this.author,
+  //     publish: this.publishedOn,
+  //   }
+  // var html = template(data)
+  // console.log(html);
+  // return html;
   //  debugger;
 };
 
