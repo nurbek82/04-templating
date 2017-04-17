29d0a8e00ddb50e6c78ee734cbd5fd07eaed8394
Nurbek Ismailov <nurbek82@gmail.com>
2017-04-16 20:39:12 -0700

    added return fn

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -15,10 +15,9 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-
-  var $source   = $('#blogTemplate').html();
-  var template = Handlebars.compile($source);
-  console.log(template);
+  var source   = $("#blogTemplate").html();
+  var template = Handlebars.compile(source);
+  // console.log(template);
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
@@ -27,11 +26,10 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
+
+
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-  // console.log (this);
-  return template(this);
-  
-  //  debugger;
+return template(this);
 };
 
 rawData.sort(function(a,b) {
